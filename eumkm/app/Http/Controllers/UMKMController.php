f582286e34bd13a2f55ce0eaf92f65198ec53b5f
MakLo1337 <mrwinataa@gmail.com>
2023-05-10 12:18:48 +0700

    Created Some Models and Databases

---
+++ eumkm/app/Http/Controllers/UMKMController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UMKM;
+use App\Http\Requests\StoreUMKMRequest;
+use App\Http\Requests\UpdateUMKMRequest;
+
+class UMKMController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUMKMRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UMKM $uMKM)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UMKM $uMKM)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUMKMRequest $request, UMKM $uMKM)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UMKM $uMKM)
+    {
+        //
+    }
+}
---
+++ eumkm/database/migrations/2023_05_10_050832_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('umkm_id');
+            $table->string('proof_payment');
+            $table->foreign('umkm_id')->references('id')->on('u_m_k_m_s')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
