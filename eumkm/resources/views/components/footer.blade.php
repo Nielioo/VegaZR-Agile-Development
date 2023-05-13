8c2b8baef4a7c799d0f48a22a02d5b68f612efaf
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2023-05-14 00:11:19 +0700

    setup font, config layout, insert logo + favicon

---
+++ eumkm/resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mt-auto bg-gray-800 text-center text-lg leading-7 font-bold text-white p-5">
+    <p>Vega ZR&nbsp;&#45;&nbsp;2023</p>
+</footer>
--- eumkm/resources/views/layouts/guest.blade.php
+++ eumkm/resources/views/layouts/guest.blade.php
@@ -1,22 +1,73 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    {{-- HyperText Markup Language Meta Tags --}}
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords"
+        content="E-UMKM, Web Application, UMKM, Agile Software Development, Informatika Universitas Ciputra Surabaya" />
+    <meta name="description"
+        content="E-UMKM adalah Aplikasi Sistem Manajemen Berbasis Web untuk Komunitas Event Organizer yang mengadakan Bazaar UMKM dengan memberikan akses kepada pelanggan dalam mengelola informasi UMKM yang telah melakukan pendaftaran untuk penyewaan tenant." />
+    <meta name="owner"
+        content="Nur Azizah, Daniel Aprillio Budiono, Mahadi Rafi Winata, Oey Darryl Valencio Wijaya, Probo Krishnacahya, Nathaniel Axel Soetrisno." />
+    <meta name="theme-color" content="#F97316">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- Title --}}
+    <title>@yield('title')</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
+    {{-- Favicon --}}
+    <link rel="icon" href="{{ url('images/favicon.svg?v=2') }}" type="image/svg" />
+
+    {{-- Laravel CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- External CSS --}}
+    <link rel="stylesheet" href="{{ url('/css/style.css') }}" type="text/css" />
+
+    {{-- Google Fonts CDN --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;600;800&display=swap" rel="stylesheet">
+
+    {{-- Alpine.js CDN --}}
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    
+    {{-- Scripts --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Styles --}}
+    @livewireStyles
+</head>
+
+<body class="font-sans antialiased">
+    <x-banner />
+
+    <div class="min-h-screen bg-gray-100">
+        {{-- Header --}}
+        @livewire('navigation-menu')
+
+        {{-- Content --}}
+        <main class="container max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 space-y-8">
+            <div class="font-sans text-gray-900 antialiased">
+                @if (isset($header))
+                    <header>
+                        {{ $header }}
+                    </header>
+                @endif
+                <br>
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+    {{-- Footer --}}
+    <x-footer />
+
+</body>
+
 </html>
