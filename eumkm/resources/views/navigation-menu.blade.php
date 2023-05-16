555dc4c98b087cda6c8e87c10e5a5f8cefe0ae67
Mahadi Rafi Winata <46525284+MakLo1337@users.noreply.github.com>
2023-05-16 17:24:40 +0700

    change navigation to events

--- eumkm/resources/views/navigation-menu.blade.php
+++ eumkm/resources/views/navigation-menu.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('event') }}" :active="request()->routeIs('event')">
                         {{ __('Events') }}
                     </x-nav-link>
                 </div>
